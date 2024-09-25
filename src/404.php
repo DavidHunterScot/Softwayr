47072b49d459974bd2730ee4cdd823ac7dd5ad7d
DavidHunterScot <david.hunter@themailbox.net>
2024-09-25 20:28:38 +0100

    Updated home page to be more fresh and modern + other tweaks

--- src/404.php
+++ src/404.php
@@ -1,22 +1,28 @@
 ---
-page_title: Not Found (404)
 friendly_urls: false
-layout: src/_includes/base.php
 ---
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<div class="w3-dark-gray w3-padding-32">
-    <div class="w3-content w3-padding">
-        <h2 class="w3-xxlarge">Not Found (404)</h2>
-        <p class="w3-large">The resource you tried to access cannot be found.</p>
-    </div>
-</div>
+        <title>404 Not Found - Softwayr</title>
 
-<div class="w3-light-gray w3-padding-64">
-    <div class="w3-content w3-padding">
-        <ul>
-            <li>Check the URL in the address bar of your web browsing software for any typing errors.</li>
-            <li>Have a look around the website to see if you can find the resource. The <a href="/">home</a> page is a good place to start.</li>
-            <li>(Optionally) If you clicked a hyperlink or button on another resource, that could now be considered outdated. Please let them know about this so it can be updated or removed accordingly.</li>
-        </ul>
-    </div>
-</div>
+        <link rel="stylesheet" href="/assets/css/stylesheet.css">
+    </head>
+
+    <body class="not_found_404">
+        <div class="container">
+            <h1><a href="/">Softwayr</a></h1>
+            <h2>404 Not Found</h2>
+            <p>The resource you tried to access cannot be found.</p>
+
+            <ul>
+                <li>Check the URL in the address bar of your web browsing software for any typing errors.</li>
+                <li>Have a look around the website to see if you can find the resource. The <a href="/">home</a> page is a good place to start.</li>
+                <li>(Optionally) If you clicked a hyperlink or button on another resource, that could now be considered outdated. Please let them know about this so it can be updated or removed accordingly.</li>
+            </ul>
+        </div>
+    </body>
+</html>
--- src/index.php
+++ src/index.php
@@ -1,16 +1,79 @@
----
-layout: src/_includes/base.php
----
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<div class="w3-metro-dark-blue w3-padding-32">
-    <div class="w3-content w3-padding">
-        <h2 class="w3-xxlarge">Welcome to Softwayr!</h2>
-        <p class="w3-large">A software development studio in Ayrshire, Scotland.</p>
-    </div>
-</div>
+        <title><?php if( isset( $metadata[ 'page_title' ] ) && $metadata[ 'page_title' ] ) echo $metadata[ 'page_title' ] . " - "; ?>Softwayr</title>
 
-<div class="w3-light-gray w3-padding-64">
-    <div class="w3-content w3-padding">
-        <p>Website currently under heavy development! Things may break or change significantly during this time. Please keep checking back to follow its development and to see what is available.</p>
-    </div>
-</div>
+        <link rel="stylesheet" type="text/css" href="/assets/css/stylesheet.css">
+    </head>
+
+    <body>
+        <a name="home"></a>
+        <section class="banner">
+            <div class="navbar">
+                <h1><a href="#"><img src="/assets/images/logos/softwayr-logo.png" alt="Softwayr" class="logo"></a></h1>
+
+                <nav>
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#contact">Contact</a>
+                </nav>
+            </div>
+
+            <div class="content">
+                <h2>WELCOME TO SOFTWAYR</h2>
+                <p>A software development studio based in Ayrshire, Scotland.</p>
+
+                <div class="buttons">
+                    <a href="#about" class="button"><span></span>Find Out More</a>
+                    <a href="#contact" class="button"><span></span>Contact</a>
+                </div>
+            </div>
+        </section>
+
+        <a name="about"></a>
+        <section class="about">
+            <div class="container">
+                <h2>About Softwayr</h2>
+                <p>The studio was created to serve as a central hub for various software-related projects and activities.</p>
+            </div>
+        </section>
+
+        <a name="projects"></a>
+        <section class="projects">
+            <div class="container">
+                <h2>Projects</h2>
+                <p>Here are some of the projects the studio has been working on.</p>
+
+                <div class="projects">
+                    <div class="project">
+                        <h3>StaticPHP</h3>
+                        <p>Fast and Simple Static Site Generator</p>
+                        <p><a href="https://staticphp.softwayr.io" target="_blank">Visit Site</a></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <a name="contact"></a>
+        <section class="contact">
+            <div class="container">
+                <h2>Contact Softwayr</h2>
+                <p>Whether you have software development idea, or want to discuss an existing project, or simply want to say hello, you are welcome to get in touch.</p>
+                <p>Best method to contact Softwayr is via email.</p>
+                <p><span class="email-address">contact [at] softwayr [dot] com</span></p>
+            </div>
+        </section>
+
+        <footer>
+            <div class="container">
+                <p>Copyright &copy; <a href="https://david-hunter.net" target="_blank">David Hunter</a>. Powered by <a href="https://staticphp.softwayr.io" target="_blank">StaticPHP</a>.</p>
+            </div>
+        </footer>
+
+        <script src="/assets/js/anti-email-harvesting.js"></script>
+    </body>
+</html>
